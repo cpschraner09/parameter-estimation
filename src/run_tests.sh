68663b925c6d3ff84d352e5449c36f5cd229ac33
drewaskeland <drewaskeland@icloud.com>
2025-03-05 01:25:03 +0000

    resolved discrepancies

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
-python3 -m unittest discover tests/
-chmod +x src/run_tests.sh
-=======
 
 #changed bash file
 cd "$(dirname "$0")/.."
 
 python3 -m unittest discover -s tests
->>>>>>> 5d5773b1ec55150147237f5a721defd6c7e02bdf
